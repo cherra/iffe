5c7fc09004a1aedfc2e6f66b25519f6b65690289
cherra <jorge@matica.mx>
2013-05-27 18:37:12 -0500

    Plantilla para facturas

--- application/controllers/preferencias/plantillas.php
+++ application/controllers/preferencias/plantillas.php
@@ -83,6 +83,40 @@ class Plantillas extends CI_Controller{
         }
         $this->load->view('preferencias/plantillas', $data);
     }
+    
+    public function facturas(){
+        $data['titulo'] = 'Facturas <small>Plantilla</small>';
+        $data['action'] = site_url('preferencias/plantillas/facturas');
+        $data['mensaje'] = '';
+        
+        if (($path = $this->configuracion->get_valor('template_path')) && (($file = $this->configuracion->get_valor('template_facturas')))){
+        
+            if(!file_exists($path)){
+                mkdir($path, 0777, true);
+            }elseif(!is_writable($path)){
+                chmod($path, 0777);
+            }
+
+            if(!file_exists($path.$file)){
+                touch($path.$file);
+            }elseif(!is_writable($path.$file)){
+                chmod($path.$file, 0777);
+            }
+
+            $this->load->helper('file');
+            if( ($datos = $this->input->post()) ){
+                if(strlen($datos['plantilla']) > 0){
+                    write_file($path.$file, $datos['plantilla']);
+                    $data['mensaje'] = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Registro exitoso</div>';
+                }
+            }
+            $data['plantilla'] = read_file($path.$file);
+            
+        }else{
+            $data['mensaje'] = '<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Aviso!</strong> No hay configuración para la plantilla de facturas.</div>';
+        }
+        $this->load->view('preferencias/plantillas', $data);
+    }
 }
 
 ?>
