bd2b67f7d97bba1cf204e15f6e4db6d28d1634a4
cherra <jorge@matica.mx>
2013-06-20 13:36:50 -0500

    Agregar plano al registrar un nuevo módulo

--- application/controllers/catalogos/ferias.php
+++ application/controllers/catalogos/ferias.php
@@ -352,6 +352,8 @@ class Ferias extends CI_Controller{
             $this->m->save($datos);
             $data['mensaje'] = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Registro exitoso</div>';
         }
+        $data['plano'] = base_url().$this->configuracion->get_valor('asset_path').$this->configuracion->get_valor('imagenes').$this->configuracion->get_valor('plano');
+        
         $this->load->view('catalogos/modulos/formulario', $data);
     }
     
