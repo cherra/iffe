709fa21a1ddaae7be01670afbea9de7d481c36a1
cherra <jorge@matica.mx>
2013-07-11 19:07:01 -0500

    Bug fix: Error en campo disponible al dibujar el plano en el home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -19,10 +19,12 @@ class Home extends CI_Controller {
             $modulos = $this->m->get_all()->result();
             $coordenadas = '';
             foreach($modulos as $modulo){
+                // Se obtiene la disponiblidad del módulo
+                $disponible = $this->m->disponible($modulo->id);
                 // Si el módulo tiene coordenadas asignadas
                 if(!empty($modulo->coordenadas))
                     if(strpos($modulo->coordenadas,'x') && strpos($modulo->coordenadas,'y')) // Se valida que el campo coordenadas contenga los caracteres 'x' y 'y'
-                        $coordenadas .= '{'.$modulo->coordenadas.',numero: "'.$modulo->numero.'",id_calle: "'.$modulo->id_calle.'", calle: "'.$modulo->calle.'", id: "'.$modulo->id.'", cliente: "'. $modulo->cliente .'", giro: "'. $modulo->giro .'", disponible: "'.$modulo->disponible.'"},';
+                        $coordenadas .= '{'.$modulo->coordenadas.',numero: "'.$modulo->numero.'",id_calle: "'.$modulo->id_calle.'", calle: "'.$modulo->calle.'", id: "'.$modulo->id.'", cliente: "'. $modulo->cliente .'", giro: "'. $modulo->giro .'", disponible: "'.$disponible.'"},';
             }
             $data['coordenadas'] = '['.substr_replace($coordenadas ,"]",-1);
             $this->load->view('home/index', $data);
